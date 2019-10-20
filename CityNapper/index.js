b40f4cdc315ab042919296a1703ecb0bfd47d200
jchaselubitz <jchaselubitz@gmail.com>
2019-10-20 23:54:55 +0200

    index.js

--- CityNapper/index.js
+++ CityNapper/index.js
@@ -2,8 +2,23 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { Navigation } from "react-native-navigation"
+// import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+//import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App)
+
+Navigation.events().registerAppLaunchedListener(() => {
+    Navigation.setRoot({
+      root: {
+        component: {
+          name: "navigation.playground.WelcomeScreen"
+        }
+      }
+    });
+  });
+
+
+
+//AppRegistry.registerComponent(appName, () => App);
