e076492f351e1e87be971b2dc49fa2b8cb9e50c8
jchaselubitz <jchaselubitz@gmail.com>
2019-11-17 14:54:30 +0100

    added mock library. failed to fix node-module error.

--- CityNapper/index.js
+++ CityNapper/index.js
@@ -3,9 +3,9 @@
  */
 
 import { Navigation } from "react-native-navigation"
-// import {AppRegistry} from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './App';
-//import {name as appName} from './app.json';
+import {name as appName} from './app.json';
 
 Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App)
 
@@ -21,4 +21,4 @@ Navigation.events().registerAppLaunchedListener(() => {
 
 
 
-//AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => App);
